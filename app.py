98bed2382fde8306f7a7254b89fbc2492bb6dc7f
Ritabrata Das <dasritabrata5@gmail.com>
2022-12-07 17:27:45 +0530

    new commit coauthored
    Co-authored-by: Ritabrata Das <dasritabrata5@gmail.com>
    Co-authored-by: Anasuya Das <ritabratadas05@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
+import requests
+import schedule
 
 app = Flask('')
 
-@app.route('/')
-def home():
-    return "Keep it alive"
+l = list()
 
 def run():
     app.run(host = '0.0.0.0', port = 8080)
@@ -13,3 +13,15 @@ def run():
 def keep_alive(): 
     t = Thread(target = run)
     t.start()
+
+def send_request():
+    URL = "https://github-readme-activity-graph.ritabratadas1.repl.co"
+    r = requests.get(url = URL)
+    l.append(str(r))
+
+@app.route('/')
+def home():
+    keep_alive()
+    schedule.every(5).seconds.do(send_request)
+    schedule.run_pending()
+    return l
